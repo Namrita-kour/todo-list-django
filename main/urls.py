edd8b53b20f9f2165a179501b87a719680470e4d
Namrita Kour <namritakours@gmail.com>
2022-01-26 18:57:46 +0530

    todo list project added

---
+++ main/urls.py
@@ -0,0 +1,27 @@
+"""todo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views 
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .import views
+
+#app_name="main"
+
+urlpatterns = [
+    path("",views.homepage, name="homepage"),
+    path('delete/<list_id>/', views.delete, name="delete"),
+    path('cross/<list_id>/', views.cross, name="cross"),
+    path('uncross/<list_id>/', views.uncross, name="uncross"),
+    path('edit/<list_id>/', views.edit, name="edit"),
+]
